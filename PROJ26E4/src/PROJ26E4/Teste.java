137a4492b1ad5b7dcf1a9baee2ec5b3b18b570ce
Sérgio Correia 53340 <53340@alunos.upt.pt>
2026-05-21 17:46:57 +0100

    Alterações

--- PROJ26E4/src/PROJ26E4/Teste.java
+++ PROJ26E4/src/PROJ26E4/Teste.java
@@ -1,7 +1,50 @@
-package PROJ26E4;
-
+package proPOO;
 public class Teste {
-	public static void main(String[]args ) {
-		System.out.println("Teste Sergio");
-	}
+    public void mostrarSistema() {
+    	
+        System.out.println("███████╗██╗███████╗████████╗███████╗███╗   ███╗ █████╗ ");
+        System.out.println("██╔════╝██║██╔════╝╚══██╔══╝██╔════╝████╗ ████║██╔══██╗");
+        System.out.println("███████╗██║███████╗   ██║   █████╗  ██╔████╔██║███████║");
+        System.out.println("╚════██║██║╚════██║   ██║   ██╔══╝  ██║╚██╔╝██║██╔══██║");
+        System.out.println("███████║██║███████║   ██║   ███████╗██║ ╚═╝ ██║██║  ██║");
+        System.out.println("╚══════╝╚═╝╚══════╝   ╚═╝   ╚══════╝╚═╝     ╚═╝╚═╝  ╚═╝");
+        System.out.println();
+        System.out.println(" ██████╗  ██████╗ ██████╗ ██████╗ ██████╗ ███████╗███╗   ██╗ ██████╗██╗ █████╗ ███████╗");
+        System.out.println("██╔═══██╗██╔════╝██╔═══██╗██╔══██╗██╔══██╗██╔════╝████╗  ██║██╔════╝██║██╔══██╗██╔════╝");
+        System.out.println("██║   ██║██║     ██║   ██║██████╔╝██████╔╝█████╗  ██╔██╗ ██║██║     ██║███████║███████╗");
+        System.out.println("██║   ██║██║     ██║   ██║██╔══██╗██╔══██╗██╔══╝  ██║╚██╗██║██║     ██║██╔══██║╚════██║");
+        System.out.println("╚██████╔╝╚██████╗╚██████╔╝██║  ██║██║  ██║███████╗██║ ╚████║╚██████╗██║██║  ██║███████║");
+        System.out.println(" ╚═════╝  ╚═════╝ ╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝  ╚═══╝ ╚═════╝╚═╝╚═╝  ╚═╝╚══════╝");
+        System.out.println();
+        System.out.println("██╗   ██╗██████╗ ████████╗");
+        System.out.println("██║   ██║██╔══██╗╚══██╔══╝");
+        System.out.println("██║   ██║██████╔╝   ██║   ");
+        System.out.println("██║   ██║██╔═══╝    ██║   ");
+        System.out.println("╚██████╔╝██║        ██║   ");
+        System.out.println(" ╚═════╝ ╚═╝        ╚═╝   ");
+        System.out.println("\n53841 - Afonso Dias");
+        System.out.println("\n53709 - David Henriques");
+        System.out.println("\n53336 - Pedro Ferreiro");
+        System.out.println("\n53297 - Rafael Viana");
+        System.out.println("\n53340 - Sérgio Correia");
+    }
+    public static void main(String[] args) {
+        Teste sistema = new Teste();
+        sistema.mostrarSistema();
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
